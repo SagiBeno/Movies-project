2c5a5a819661a60e66d8ba56383f111612b77ed8
hajos8 <rabb.andras@gmail.com>
2025-11-26 14:45:57 +0100

    Implemented search for searchpage

--- react-app/src/pages/SearchPage.jsx
+++ react-app/src/pages/SearchPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { PageLayout } from '../components/PageLayout'
-import { Grid, TextField } from '@mui/material'
+import { Box, Grid, TextField, ToggleButtonGroup, ToggleButton, Button } from '@mui/material'
 import { MyCard } from '../components/MyCard'
 import { MySpinner } from '../components/MySpinner'
+import { FaSearch } from "react-icons/fa";
+
+import { getSearchData } from '../utils'
 
 import { useState } from 'react'
 
@@ -11,12 +14,47 @@ export const SearchPage = () => {
   const [selectedGenres, setSelectedGenres] = useState([])
   const [data, setData] = useState([])
   const [isLoading, setLoading] = useState(false)
+  const [selectedType, setSelectedType] = useState('movie')
+  const [query, setQuery] = useState('')
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  }
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setData(await getSearchData({ queryKey: ['search', selectedType, query, page] }));
+    setLoading(false);
+  }
+
+  const handleTypeChange = (event, newType) => {
+    if (newType != null) {
+      setSelectedType(newType);
+    }
+  }
 
   return (
-    <PageLayout title="Search page" page={page} setPage={se} type='movie'
-      selectedGenres={[]} setSelectedGenres={() => { }} data={data}
+    <PageLayout title="Search page" page={page} setPage={setPage} type={selectedType}
+      selectedGenres={selectedGenres} setSelectedGenres={setSelectedGenres} data={data}
     >
-      <TextField id="outlined-basic" label="Search" variant="outlined" />
+      <Box mb={2} display="flex" justifyContent="center">
+        <TextField id="outlined-basic" label="Search" variant="outlined" onChange={handleInputChange}/>
+        <Button variant="outlined" onClick={handleSearch}><FaSearch /></Button>
+      </Box>
+      <Box mb={2} display="flex" justifyContent="center">
+        <ToggleButtonGroup
+          color="primary"
+          value={selectedType}
+          exclusive
+          onChange={handleTypeChange}
+          aria-label="Type Selection"
+        >
+          <ToggleButton value="movie">Movie</ToggleButton>
+          <ToggleButton value="tv">TV</ToggleButton>
+        </ToggleButtonGroup>
+      </Box>
+
       {isLoading && <MySpinner />}
       <Grid>
         {data && data.results?.map(obj =>
